6e5d939cb82abd6a8bb75624a014680626463ee6
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-08 12:51:32 -0300

    Base Model Task6 > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- models/base_model.py
+++ models/base_model.py
@@ -26,14 +26,17 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            #storage.new(self) -> Moved
+            #storage.new(self) Moved
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value,'%Y-%m-%dT%H:%M:%S.%f')
+                    
+                if key != __class__:
+                    setattr(self, key, value)
+
+            # del kwargs['__class__']
+            # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -55,4 +58,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         return dictionary
+
+    def delete(self):
+        """to delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
+        storage.save()
