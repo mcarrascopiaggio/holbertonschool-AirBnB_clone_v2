1a01ae89ae4be4631d2a8709dd9e985240b832c1
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-08 12:30:27 -0300

    sql Scripts Created > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Setup Script
+-- Write a script that prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
