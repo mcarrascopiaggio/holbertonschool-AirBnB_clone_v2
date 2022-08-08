b34b9bb5c423860ab0e058eedc650886360bad51
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-08 12:41:16 -0300

    Some Changes > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,22 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    
+    id = Column(String(60), nullable=False, primary_key=True)
+    
+    created_at = Column(DateTime(default=datetime.utcnow()),
+                        nullable=False)
+    
+    updated_at = Column(DateTime(default=datetime.utcnow()),
+                        nullable=False)
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -16,7 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            #storage.new(self) -> Moved
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -34,6 +44,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
