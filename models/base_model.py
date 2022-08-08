f231f705dc83f11343d58ef248974548817c2bd4
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-08-08 08:45:30 -0700

    Personal Test. > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,10 @@ class BaseModel:
     id = Column(String(60), nullable=False, primary_key=True)
     
     created_at = Column(DateTime(default=datetime.utcnow()),
-                        nullable=False)
+                       nullable=False)
     
     updated_at = Column(DateTime(default=datetime.utcnow()),
-                        nullable=False)
+                       nullable=False)
     
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
