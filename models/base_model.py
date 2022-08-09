a2152b5cb9aa5585d31c3ea2811671582e09827d
root <mcarrascopiaggio@gmail.com>
2022-08-08 19:14:37 -0700

    do_all works with Class in Int.mode > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value,'%Y-%m-%dT%H:%M:%S.%f')
                     
-                if key != __class__:
+                if key != '__class__':
                     setattr(self, key, value)
 
             # del kwargs['__class__']
