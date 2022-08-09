2691278ce1bb0268b1251ba72f8ca58c9079aac0
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-09 16:08:37 -0300

    Test State > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,6 +2,9 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import unittest
+import models
+from models.base_model import BaseModel, Base
 
 
 class test_state(test_basemodel):
@@ -17,3 +20,35 @@ class test_state(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_documentation(self):
+        """
+        check if class has documentation
+        """
+        self.assertIsNotNone(State.__doc__)
+
+    def test_class(self):
+        """
+        test class & test subclass
+        """
+        self.assertEqual(State.name, "")
+        self.assertTrue(issubclass(State, BaseModel))
+
+    def test_instance(self):
+        """
+        test instance of state
+        """
+        test = State()
+        self.assertEqual(test.name, "")
+        self.assertTrue(issubclass(State, BaseModel))
+
+    def test_attributetype(self):
+        """
+        test attribute test
+        """
+        test = State()
+        self.assertEqual(type(test.name), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
