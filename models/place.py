be0517d69372b96e5233f065c22d7bc20e43898d
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-09 16:03:23 -0300

    Pycode Style > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- models/place.py
+++ models/place.py
@@ -74,7 +74,7 @@ class Place(BaseModel, Base):
 place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
-                      Column("amenity_id", String(60), 
+                      Column("amenity_id", String(60),
                              ForeignKey("amenities.id"), primary_key=True,
                              nullable=False)
                       )
