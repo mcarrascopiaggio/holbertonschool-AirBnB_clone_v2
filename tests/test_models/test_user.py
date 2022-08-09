fda0e5b8df624827536743f3703742452772e0d8
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-09 16:09:37 -0300

    Test User > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,9 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+import unittest
+import models
+from models.base_model import BaseModel, Base
 
 
 class test_User(test_basemodel):
@@ -32,3 +35,34 @@ class test_User(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.password), str)
+        
+    def test_documentation(self):
+        """
+        check if class has documentation
+        """
+        self.assertIsNotNone(User.__doc__)
+
+    def test_class(self):
+        """
+        test class & test subclass
+        """
+        self.assertEqual(User.email, "")
+        self.assertEqual(User.password, "")
+        self.assertEqual(User.first_name, "")
+        self.assertEqual(User.last_name, "")
+        self.assertTrue(issubclass(User, BaseModel))
+
+    def test_instance(self):
+        """
+        test instance of User
+        """
+        test = User()
+        self.assertEqual(test.email, "")
+        self.assertEqual(test.password, "")
+        self.assertEqual(test.first_name, "")
+        self.assertEqual(test.last_name, "")
+        self.assertTrue(issubclass(User, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
