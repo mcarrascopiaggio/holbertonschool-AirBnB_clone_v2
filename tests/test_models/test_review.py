09403435857b479f33addeb8a475401e540004eb
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-09 16:07:22 -0300

    Test Review > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,6 +2,9 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import unittest
+import models
+from models.base_model import BaseModel, Base
 
 
 class test_review(test_basemodel):
@@ -27,3 +30,41 @@ class test_review(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.text), str)
+    
+    def test_documentation(self):
+        """
+        check if class has documentation
+        """
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_class(self):
+        """
+        test class & test subclass
+        """
+        self.assertEqual(Review.place_id, "")
+        self.assertEqual(Review.user_id, "")
+        self.assertEqual(Review.text, "")
+        self.assertTrue(issubclass(Review, BaseModel))
+
+    def test_instance(self):
+        """
+        test instance of state
+        """
+        test = Review()
+        self.assertEqual(test.place_id, "")
+        self.assertEqual(test.user_id, "")
+        self.assertEqual(test.text, "")
+        self.assertTrue(issubclass(Review, BaseModel))
+
+    def test_attributetype(self):
+        """
+        test attribute test
+        """
+        test = Review()
+        self.assertEqual(type(test.place_id), str)
+        self.assertEqual(type(test.user_id), str)
+        self.assertEqual(type(test.text), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
