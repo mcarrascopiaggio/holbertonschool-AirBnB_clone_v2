32b2c623db0ad6b5d0a394060aeeb22f4de4794c
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-09 16:18:32 -0300

    Test DBStorage > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module for testing file storage"""
+import unittest
+from models.engine.db_storage import DBStorage
+
+
+class test_dbStorage(unittest.TestCase):
+    """ Class to test the file storage method """
