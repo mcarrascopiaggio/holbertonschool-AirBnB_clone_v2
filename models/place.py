4b3522d446d1b53303e29cc9a6c5ed2cebf44773
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-09 16:02:57 -0300

    Pycode Style > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- models/place.py
+++ models/place.py
@@ -71,9 +71,10 @@ class Place(BaseModel, Base):
                         amenity_ids.append(value)
 
 
-place_amenity = Table("place_amenity", Base.metadata, 
+place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
-                      Column("amenity_id", String(60), ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False)
+                      Column("amenity_id", String(60), 
+                             ForeignKey("amenities.id"), primary_key=True,
+                             nullable=False)
                       )
