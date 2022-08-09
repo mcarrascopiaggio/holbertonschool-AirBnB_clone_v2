212b41fe15919a6ad29d5e3b80410e57c32c1cd7
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-09 15:41:06 -0300

    Test Amenity > > Co-authored-by: MarcelaCarrasco<mcarrascopiaggio@gmail.com> Co-authored-by: JuanManuelReyes<juanmanuelreyesarrambide@gmail.com>

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" """
+""" test Class Amenity """
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+import models
 
 
 class test_Amenity(test_basemodel):
@@ -12,8 +15,37 @@ class test_Amenity(test_basemodel):
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
+    
+    def test_documentation(self):
+        """
+        check if class has documentation
+        """
+        self.assertIsNotNone(Amenity.__doc__)
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_class(self):
+        """
+        test class & test subclass
+        """
+        self.assertEqual(Amenity.name, "")
+        self.assertTrue(issubclass(Amenity, BaseModel))
+        self.assertTrue(issubclass(Amenity, Base))
+
+    def test_instance(self):
+        """
+        test instance of state
+        """
+        test = Amenity()
+        self.assertEqual(test.name, "")
+        self.assertTrue(issubclass(Amenity, BaseModel))
+        self.assertTrue(issubclass(Amenity, Base))
+
+    def test_attributetype(self):
+        """
+        test attribute test
+        """
+        test = Amenity()
+        self.assertEqual(type(test.name), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
