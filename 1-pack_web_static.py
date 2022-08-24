afc1c5f466c9b9a8921427a89458e793abcdfb06
root <mcarrascopiaggio@gmail.com>
2022-08-24 15:00:08 -0700

    fix pep 8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """
-0x03. AirBnB clone - Deploy static
-task 1. Compress before sending
 Fabric script that generates a .tgz archive from the contents of the
 web_static folder of your AirBnB Clone repo, using the function do_pack
 https://www.hostinger.es/tutoriales/como-usar-comando-tar-linux
@@ -9,7 +7,6 @@ https://www.hostinger.es/tutoriales/como-usar-comando-tar-linux
 
 from fabric.api import local
 from datetime import datetime
-import os
 
 
 def do_pack():
@@ -17,8 +14,8 @@ def do_pack():
     local("mkdir -p versions")
     now = datetime.now()
     name = "versions/web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
-try:
+    try:
         local("tar -czvf " + name + " web_static")
         return name
-    except:
+    except as exception:
         return None
