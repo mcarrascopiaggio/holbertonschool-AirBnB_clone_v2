e6218b3dd9c13eb6bd21ac399e08e0244ce29d0c
root <mcarrascopiaggio@gmail.com>
2022-08-24 20:42:55 -0700

    try again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,23 +9,24 @@ from os import path
 env.hosts = ['54.87.154.255', '54.172.224.147']
 env.user = "ubuntu"
 
+
 def do_deploy(archive_path):
     """function to distribute an archive to web server"""
     if not (path.exists(archive_path)):
         return False
     try:
-archive_nom = archive_path.split('/')[1]
-    archive_nom_noext = archive_path.split('/')[1].split('.')[0]
-    to_path = '/data/web_static/releases/' + archive_nom_noext
-    up_path = '/tmp/' + archive_nom
-    put(archive_path, up_path)
-    run('mkdir -p ' + to_path)
-    run('tar -xzf ' + up_path + ' -C ' + to_path)
-    run('rm ' + up_path)
-    run('mv ' + to_path + '/web_static/* ' + to_path + '/')
-    run('rm -rf ' + to_path + '/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + to_path + ' /data/web_static/current')
-    return True
-    except:
+        file_name = archive_path.split('/')[1]
+        file_name_noext = archive_path.split('/')[1].split('.')[0]
+        to_path = '/data/web_static/releases/' + file_name_noext
+        up_path = '/tmp/' + file_name
+        put(archive_path, up_path)
+        run('mkdir -p ' + to_path)
+        run('tar -xzf ' + up_path + ' -C ' + to_path)
+        run('rm ' + up_path)
+        run('mv ' + to_path + '/web_static/* ' + to_path + '/')
+        run('rm -rf ' + to_path + '/web_static')
+        run('rm -rf /data/web_static/current')
+        run('ln -s ' + to_path + ' /data/web_static/current')
+        return True
+    except Exception:
         return False
