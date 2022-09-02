5cac82cb11c816c0ef3b4b126fa6ff56cb53622c
root <mcarrascopiaggio@gmail.com>
2022-09-02 11:07:09 -0700

    still can not test anything :(

---
+++ web_flask/9-states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:
+You must use storage for fetching data from the storage engine (FileStorage or
+DBStorage) => from models import storage and storage.all(...)
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes: /states_list: display a HTML page: (inside the tag BODY)
+
+"""
+from flask import Flask, render_template
+from models import storage
+from models import state
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """Closing DB"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    """
+        Displays the list of the Cities
+    """
+    states = storage.all(State).values()
+    if id is not None:
+        for state in states:
+            if state.id == id:
+                return render_template('9-states.html', states=state)
+        return render_template('9-states.html')
+    return render_template('9-states.html', states=states, full=True)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
