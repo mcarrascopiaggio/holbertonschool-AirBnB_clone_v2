e69c8e51f669d1ba898c8285a5188d02a0e8a5b7
root <mcarrascopiaggio@gmail.com>
2022-09-02 10:27:50 -0700

    add some code but can not test

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:
+You must use storage for fetching data from the storage engine (FileStorage or
+DBStorage) => from models import storage and storage.all(...)
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes: /states_list: display a HTML page: (inside the tag BODY)
+
+"""
+from os import stat
+from flask import Flask, render_template
+from models import storage
+from models import state
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """Closing DB"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displaying list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
